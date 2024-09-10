b3ddaf8b0962b7328cb601d878213e49a1d6d64d
Habib <habibullah.2004@yahoo.com>
2024-09-10 13:46:25 +0430

    Built the foundation

--- view/urls.py
+++ view/urls.py
@@ -4,4 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index),
+    path('product/<str:slug>/', views.product_detail, name='product_detail'),
+    path('products', views.products, name='all_products'),
+    path('legal', views.legal, name='legal'),
 ]
--- view/views.py
+++ view/views.py
@@ -3,3 +3,12 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hola cabrone")
+
+def product_detail(request, slug):
+    return HttpResponse("Product")
+
+def products(request):
+    return HttpResponse("Products")
+
+def legal(request):
+    return HttpResponse("Legal")
