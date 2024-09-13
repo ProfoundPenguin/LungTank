1c68fd5057042c6dc0ed4872a21ad53f4da15f27
Habib <habibullah.2004@yahoo.com>
2024-09-13 14:14:31 +0430

    Daily Commit

--- static/script/effects.js
+++ static/script/effects.js
@@ -5,14 +5,25 @@ const banner_bar = document.querySelector('#banner #main_bar')
 pc_menu.forEach(element => {
     element.addEventListener('mouseover', ()=> {
         element.style.color = "#000000"
-        hover_effect.style.opacity = "1"
+        // hover_effect.style.opacity = "1"
 
-        let position = element.getBoundingClientRect().left - banner_bar.getBoundingClientRect().left + (element.offsetWidth)
+        let position = element.getBoundingClientRect().left - banner_bar.getBoundingClientRect().left
         
-        hover_effect.style.transform = "translateX(" + position + "px)"
+        hover_effect.style.width = (element.offsetWidth + 0) + "px"
+
+        
+
+        hover_effect.style.transform = "translateX(" + (position - 0) + "px)"
+        // setTimeout(()=> {
+        //     hover_effect.style.transition = "0.2s"
+        // }, 100)
         
     })
     element.addEventListener('mouseleave', ()=> {
         element.style.color = "#CDE8E5"
     })
 });
+
+banner_bar.addEventListener('mouseleave', ()=> {
+    // hover_effect.style.transition = "0s"
+})
